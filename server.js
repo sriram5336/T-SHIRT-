db1c517d45f54b92daad32db09d6bb0e5e784cc6
sriram5336 <sriramtrichy315@gmail.com>
2026-08-16 09:32:00 +0530

    as

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const MIME_TYPES = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.png': 'image/png',
+  '.svg': 'image/svg+xml',
+  '.json': 'application/json'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, decodeURIComponent(req.url === '/' ? 'index.html' : req.url));
+  let ext = path.extname(filePath).toLowerCase();
+  let contentType = MIME_TYPES[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>', 'utf-8');
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`SmartBill Apparel Server running at http://localhost:${PORT}/`);
+});
